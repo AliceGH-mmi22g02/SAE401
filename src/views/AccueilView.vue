a0e8ea5d56aadba72ea569ba2b6517cba7db16cb
Maëve Basque <maevebsq@gmail.com>
2024-03-15 14:42:50 +0100

    ajout fichier scss3

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -5,41 +5,57 @@ import '/public/css/_global.scss'
 <template>
   <main>
     <h1>Acceuil </h1>
+    <section class="bienvenue">
     <h2>Bienvenue sur <strong>GREENPLAY</strong> !</h2>
-    <p>Plongez dans notre jeu interactif et ludique où chaque quête nous rapproche d'un avenir plus vert. Apprenez, jouez, gagnez des récompenses et contribuez à la préservation de notre planète.</p>
+      <p>Plongez dans notre jeu interactif et ludique où chaque quête nous rapproche d'un avenir plus vert. Apprenez, jouez, gagnez des récompenses et contribuez à la préservation de notre planète.</p>
     <br>
+    </section>
+    <section class="pourquoi-inscrire">
     <h2>Pourquoi s’inscrire ?</h2>
-    <p>S'inscrire sur <strong>GREENPLAY</strong>, c'est rejoindre une communauté engagée dans la lutte pour un avenir durable. En créant un compte, vous pourrez suivre vos progrès, sauvegarder vos quêtes achevées et accumuler des récompenses exclusives.</p>
+     <p>S'inscrire sur <strong>GREENPLAY</strong>, c'est rejoindre une communauté engagée dans la lutte pour un avenir durable. En créant un compte, vous pourrez suivre vos progrès, sauvegarder vos quêtes achevées et accumuler des récompenses exclusives.</p>
     <br>
+    </section>
+    <section class="pourquoi-jouer">
     <h2>Pourquoi jouer ?</h2>
-    <p>Jouer sur <strong>GREENPLAY</strong> est une opportunité unique de combiner amusement et sensibilisation aux enjeux écologiques. C'est une manière engageante d'apprendre sur les défis environnementaux tout en découvrant des moyens concrets d'y répondre. À travers chaque quête, vous augmentez non seulement votre connaissance sur l'écologie, mais vous contribuez aussi activement à des initiatives de préservation de l'environnement.</p>
+      <p>Jouer sur <strong>GREENPLAY</strong> est une opportunité unique de combiner amusement et sensibilisation aux enjeux écologiques. C'est une manière engageante d'apprendre sur les défis environnementaux tout en découvrant des moyens concrets d'y répondre. À travers chaque quête, vous augmentez non seulement votre connaissance sur l'écologie, mais vous contribuez aussi activement à des initiatives de préservation de l'environnement.</p>
     <br>
+    </section>
+    <section class="comment-jouer">
     <h2>Comment jouer ?</h2>
-    <p>1. Inscrivez-vous pour suivre vos progrès<br>
+      <p>1. Inscrivez-vous pour suivre vos progrès<br>
       2. Explorez et sélectionnez une quête depuis le tableau des quêtes.<br>
       3. Réalisez les objectifs de la quête, validez votre succès en répondant à un quizz,<br> et récoltez des récompenses depuis votre compte<br>
-    </p>
+      </p>
     <br>
-    <h2>Sujet abordés des Quêtes</h2>
+    </section>
+    <section class="sujets">
+    <h2>Sujets abordés des Quêtes</h2>
     <br>
+    </section>
+    <section class="quete">
     <h2>A quoi ressemble une Quête ?</h2>
-    <p>- Dans le pop-up de quête, découvrez une courte description du problème environnemental.</p>
-    <p>- Découvrez la quête principale et une quête secondaire optionnelle à intégrer dans votre quotidien (la non-validation de celle-ci n’empêche pas la validation de la quête principale).</p>
-    <p>- Cliquez sur "Effectuer la Quête" pour accéder à la page d'information et commencez votre quête !</p>
+      <p>- Dans le pop-up de quête, découvrez une courte description du problème environnemental.</p>
+      <p>- Découvrez la quête principale et une quête secondaire optionnelle à intégrer dans votre quotidien (la non-validation de celle-ci n’empêche pas la validation de la quête principale).</p>
+      <p>- Cliquez sur "Effectuer la Quête" pour accéder à la page d'information et commencez votre quête !</p>
     <br>
+    </section>
+    <section class="recompense">
     <h2>Système de Récompense</h2>
-    <p><strong>Soyez récompensés à chaque fin de quête !</strong></p>
+      <p><strong>Soyez récompensés à chaque fin de quête !</strong></p>
     <br>
-    <p><strong>Badges de réussite :</strong> À l'issue de chaque quête, vous recevez un badge unique. Ces badges symbolisent les connaissances que vous avez acquises et votre participation active à la cause écologique.</p>
+      <p><strong>Badges de réussite :</strong> À l'issue de chaque quête, vous recevez un badge unique. Ces badges symbolisent les connaissances que vous avez acquises et votre participation active à la cause écologique.</p>
     <br>
-    <p><strong>Codes promotionnels :</strong> Profitez de codes promotionnels pour des jeux vidéo éco-responsables !</p>
+      <p><strong>Codes promotionnels :</strong> Profitez de codes promotionnels pour des jeux vidéo éco-responsables !</p>
     <br>
-    <p><strong>Une fois toutes les quêtes terminées, une <strong>surprise</strong> vous attend !</strong></p>
+      <p><strong>Une fois toutes les quêtes terminées, une <strong>surprise</strong> vous attend !</strong></p>
     <br>
+    </section>
+    <section class="accueilfooter">
     <h2>Au-delà des récompenses virtuelles, votre participation contribue réellement à la protection de l'environnement.</h2>
     <br>
-    <p>Toutes vos récompenses sont sauvegardées sur votre profil dès l'inscription.</p>
+      <p>Toutes vos récompenses sont sauvegardées sur votre profil dès l'inscription.</p>
     <br>
-    <p>Rejoignez l'aventure <strong>GREENPLAY</strong> ! Apprenez, agissez, et soyez récompensés :)</p>
+      <p>Rejoignez l'aventure <strong>GREENPLAY</strong> ! Apprenez, agissez, et soyez récompensés :)</p>
+    </section>
   </main>
 </template>
