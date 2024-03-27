5e048b07d0d6245ffe47a379857765015e595ee7
Ghantfasia <aliceghanimi@hotmail.com>
2024-03-27 10:45:28 +0100

    Css accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -24,56 +24,50 @@ export default {
 
 <template>
 
-  <div ref="myElement">
-    <h1>SECTION TEST</h1>
-    <p>{{ $t("main.p3") }}</p>
-    <h1>FIN SECTION TEST</h1>
-  </div>
   <main>
+    <div class="banniere" style="background-color: #5E807F; height: 350px"><img src=""></div>
 
-    <LocaleChanger/>
-    <h1>{{ $t("main.home") }}</h1>
-    <h2>{{ $t("main.welcome") }} <strong>{{ $t("special.gp")}}</strong> {{ $t("special.exclam")}}</h2>
-    <p>{{ $t("main.p1") }}</p>
-    <br>
-    <h2>{{ $t("main.why") }}</h2>
-    <p>{{ $t("main.p2") }}</p>
-    <br>
-    <h2>{{ $t("main.whyplay") }}</h2>
-    <p>{{ $t("main.p3") }}</p>
-    <br>
-    <h2>{{ $t("main.how") }}</h2>
-    <p>{{ $t("main.p4-1") }}</p>
-    <p>{{ $t("main.p4-2") }}</p>
-    <p>{{ $t("main.p4-3") }}</p>
-    <br>
-    <h2>{{ $t("main.subject") }}</h2>
-    <br>
-    <h2>{{ $t("main.what") }}</h2>
-    <p>{{ $t("main.p5") }}</p>
-    <p>{{ $t("main.p6") }}</p>
-    <p>{{ $t("main.p7") }}</p>
-    <br>
-    <h2>{{ $t("main.rewards") }}</h2>
-    <p>{{ $t("main.p8") }}</p>
-    <br>
-    <p>{{ $t("main.p9") }}</p>
-    <br>
-    <p>{{ $t("main.p10") }}</p>
-    <br>
-    <p>{{ $t("main.p11") }}</p>
-    <br>
-    <h2>{{ $t("main.more") }}</h2>
-    <br>
-    <p>{{ $t("main.p12") }}</p>
-    <br>
-    <p>{{ $t("main.p13") }}</p>
+    <div class="intro" style="background-color:#35e2a6">
+      <h1>{{ $t("main.welcome") }} <strong>{{ $t("special.gp")}}</strong> {{ $t("special.exclam")}}</h1>
+      <p>{{ $t("main.p1") }}</p>
 
-    <section>
-      <h3>Au-delà des récompenses virtuelles, votre participation contribue réellement à la protection de l'environnement.</h3>
+      <h2>{{ $t("main.why") }}</h2>
+      <p>{{ $t("main.p2") }}</p>
+
+      <h2>{{ $t("main.whyplay") }}</h2>
+      <p>{{ $t("main.p3") }}</p>
+    </div>
+
+    <div class="tuto" style="background-color:#35e2a6">
+      <h2>{{ $t("main.how") }}</h2>
+      <p>{{ $t("main.p4-1") }}</p>
+      <p>{{ $t("main.p4-2") }}</p>
+      <p>{{ $t("main.p4-3") }}</p>
+
+      <h2>{{ $t("main.what") }}</h2>
+      <p>{{ $t("main.p5") }}</p>
+      <p>{{ $t("main.p6") }}</p>
+      <p>{{ $t("main.p7") }}</p>
+    </div>
+
+    <div class="questsubject" style="background-color:#35e2a6">
+      <h2>{{ $t("main.subject") }}</h2>
+    </div>
+
+    <div class="tuto" style="background-color:#35e2a6">
+      <h2>{{ $t("main.rewards") }}</h2>
+      <p>{{ $t("main.p8") }}</p>
+      <p>{{ $t("main.p9") }}</p>
+      <p>{{ $t("main.p10") }}</p>
+      <p>{{ $t("main.p11") }}</p>
+    </div>
+
+    <div class="tuto" style="background-color:#35e2a6">
+      <h2>{{ $t("main.more") }}</h2>
       <br>
-      <h3>Alors rejoignez l'aventure GREENPLAY ! Apprenez, agissez, et soyez récompensées :)</h3>
-    </section>
-
+      <p>{{ $t("main.p12") }}</p>
+      <br>
+      <p>{{ $t("main.p13") }}</p>
+    </div>
   </main>
 </template>
