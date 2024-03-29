6f3b5fa3e10ae6bced979b0e69e52c8ec331d37c
Ghantfasia <aliceghanimi@hotmail.com>
2024-03-29 15:45:40 +0100

    Css accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -27,27 +27,38 @@ export default {
   <main>
     <div class="banniere" style="background-color: #5E807F; height: 350px"><img src=""></div>
 
-    <div class="intro" style="background-color:#35e2a6">
-      <h1>{{ $t("main.welcome") }} <strong>{{ $t("special.gp")}}</strong> {{ $t("special.exclam")}}</h1>
-      <p>{{ $t("main.p1") }}</p>
+    <div class="intro">
+      <div class="wellcome">
+        <h1>{{ $t("main.welcome") }} <strong>{{ $t("special.gp")}}</strong> {{ $t("special.exclam")}}</h1>
+        <p>{{ $t("main.p1") }}</p>
+      </div>
 
-      <h2>{{ $t("main.why") }}</h2>
-      <p>{{ $t("main.p2") }}</p>
-
-      <h2>{{ $t("main.whyplay") }}</h2>
-      <p>{{ $t("main.p3") }}</p>
+      <div class="why">
+        <div class="why-sign">
+          <h2>{{ $t("main.why") }}</h2>
+          <p>{{ $t("main.p2") }}</p>
+        </div>
+        <div class="why-play">
+          <h2>{{ $t("main.whyplay") }}</h2>
+          <p>{{ $t("main.p3") }}</p>
+        </div>
+      </div>
     </div>
 
     <div class="tuto" style="background-color:#35e2a6">
       <h2>{{ $t("main.how") }}</h2>
-      <p>{{ $t("main.p4-1") }}</p>
-      <p>{{ $t("main.p4-2") }}</p>
-      <p>{{ $t("main.p4-3") }}</p>
+      <div class="how" style="background-color: #5E807F">
+        <p>{{ $t("main.p4-1") }}</p>
+        <p>{{ $t("main.p4-2") }}</p>
+        <p>{{ $t("main.p4-3") }}</p>
+      </div>
 
       <h2>{{ $t("main.what") }}</h2>
-      <p>{{ $t("main.p5") }}</p>
-      <p>{{ $t("main.p6") }}</p>
-      <p>{{ $t("main.p7") }}</p>
+      <div class="what" style="background-color: #5E807F">
+        <p>{{ $t("main.p5") }}</p>
+        <p>{{ $t("main.p6") }}</p>
+        <p>{{ $t("main.p7") }}</p>
+      </div>
     </div>
 
     <div class="questsubject" style="background-color:#35e2a6">
