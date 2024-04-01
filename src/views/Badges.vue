39b43837b47ade4605e0d1b80f4b104929b40366
Maëve Basque <maevebsq@gmail.com>
2024-04-01 16:28:12 +0200

    correction erreur InformationView

--- src/views/Badges.vue
+++ src/views/Badges.vue
@@ -28,5 +28,42 @@
       <p class="text">4. Chevalier de la Faune :<br>
         Ce badge est décerné après la quête numéro 4, "L’augmentation du nombre d’espèces en voie de disparition". Il souligne les efforts pour protéger les espèces menacées et maintenir la diversité biologique.</p>
     </div>
+
+
+    <div class="rounded-box">
+      <img class="rounded-image" src="/src/assets/profil.png" alt="Image">
+      <p class="text">5. Protecteur des Richesses Terrestres :<br>
+        Obtenu à l'issue de la quête numéro 5, "Surconsommation des ressources naturelles". Ce badge valorise les actions pour conserver les ressources de la Terre et promouvoir un usage responsable.</p>
+    </div>
+
+    <div class="rounded-box">
+      <img class="rounded-image" src="/src/assets/profil.png" alt="Image">
+      <p class="text">6. Purificateur Atmosphérique :<br>
+        Attribué après la quête numéro 6, "La crise de la pollution". Il récompense ceux qui adoptent des pratiques réduisant la pollution et contribuent à un environnement plus sain.</p>
+    </div>
+
+    <div class="rounded-box">
+      <img class="rounded-image" src="/src/assets/profil.png" alt="Image">
+      <p class="text">7. La vie protège la vie :<br>
+        Décerné suite à la quête numéro 7, "Menaces sur la biodiversité". Ce badge est une reconnaissance pour ceux qui s'engagent à préserver la biodiversité par des choix respectueux de l'environnement.</p>
+    </div>
+
+    <div class="rounded-box">
+      <img class="rounded-image" src="/src/assets/profil.png" alt="Image">
+      <p class="text">8. Nettoyeur Planétaire :<br>
+        Obtenu après la quête numéro 8, "Le défi de la gestion des déchets". Il met en lumière l'importance du tri et du recyclage pour réduire l'impact environnemental des déchets.</p>
+    </div>
+
+    <div class="rounded-box">
+      <img class="rounded-image" src="/src/assets/profil.png" alt="Image">
+      <p class="text">9. Allié des Océans :<br>
+        Ce badge est attribué à la fin de la quête numéro 9, "La surexploitation des ressources marines". Il honore ceux qui soutiennent la vie marine et la pêche durable pour protéger les océans.</p>
+    </div>
+
+    <div class="rounded-box">
+      <img class="rounded-image" src="/src/assets/profil.png" alt="Image">
+      <p class="text">10. Cultivateur des Terres Fertiles :<br>
+        Décerné après la quête numéro 10, "La dégradation et l'épuisement des sols". Ce badge valorise le soutien à l'agriculture durable et les efforts pour préserver la fertilité des sols.</p>
+    </div>
   </main>
 </template>
