ab965a18836aa1089295875cee47698051e11e38
SomebirdFaust <faust.gaulard@gmail.com>
2024-04-10 10:35:36 +0200

    locale dans la nav

--- src/views/Badges.vue
+++ src/views/Badges.vue
@@ -5,65 +5,64 @@
 <script setup>
 </script>
 <template>
-  <h1 class="h1badges">Liste des badges</h1>
+  <h1 class="h1badges">{{ $t("badges.h1") }}</h1>
   <main id="main-badges">
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge1.svg" alt="Image">
-        <p class="text">1. Gardien de la forêt :<br>
-          Ce badge peut être obtenu après avoir fini la quête numéro 1, "La lutte contre la déforestation". Il récompense ceux qui contribuent à la préservation des forêts, essentielles pour la biodiversité et le climat de notre planète.</p>
+        <p class="text">{{ $t("badges.p1") }}</p>
+        <p class="text">{{ $t("badges.p1suite") }}</p>
     </div>
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge2.svg" alt="Image">
-      <p class="text">2. Sentinelle du Climat :<br>
-        Obtenu après la quête numéro 2, "Les dangers des risques climatiques". Ce badge distingue ceux qui agissent pour atténuer les risques liés au changement climatique en améliorant l'efficacité énergétique de leur domicile.
-      </p>
+      <p class="text">{{ $t("badges.p2") }}</p>
+      <p class="text">{{ $t("badges.p2suite") }}</p>
     </div>
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge3.svg" alt="Image">
-      <p class="text">3. Pompier du climat :<br>
-        Attribué suite à la quête numéro 3, "Les causes du réchauffement climatique". Il honore l'engagement à réduire les émissions de gaz à effet de serre et à lutter contre le réchauffement de la planète.</p>
+      <p class="text">{{ $t("badges.p3") }}</p>
+      <p class="text">{{ $t("badges.p3suite") }}</p>
     </div>
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge4.svg" alt="Image">
-      <p class="text">4. Chevalier de la Faune :<br>
-        Ce badge est décerné après la quête numéro 4, "L’augmentation du nombre d’espèces en voie de disparition". Il souligne les efforts pour protéger les espèces menacées et maintenir la diversité biologique.</p>
+      <p class="text">{{ $t("badges.p4") }}</p>
+      <p class="text">{{ $t("badges.p4suite") }}</p>
     </div>
 
 
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge5.svg" alt="Image">
-      <p class="text">5. Protecteur des Richesses Terrestres :<br>
-        Obtenu à l'issue de la quête numéro 5, "Surconsommation des ressources naturelles". Ce badge valorise les actions pour conserver les ressources de la Terre et promouvoir un usage responsable.</p>
+      <p class="text">{{ $t("badges.p5") }}</p>
+      <p class="text">{{ $t("badges.p5suite") }}</p>
     </div>
 
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge6.svg" alt="Image">
-      <p class="text">6. Purificateur Atmosphérique :<br>
-        Attribué après la quête numéro 6, "La crise de la pollution". Il récompense ceux qui adoptent des pratiques réduisant la pollution et contribuent à un environnement plus sain.</p>
+      <p class="text">{{ $t("badges.p6") }}</p>
+      <p class="text">{{ $t("badges.p6suite") }}</p>
     </div>
 
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge7.svg" alt="Image">
-      <p class="text">7. La vie protège la vie :<br>
-        Décerné suite à la quête numéro 7, "Menaces sur la biodiversité". Ce badge est une reconnaissance pour ceux qui s'engagent à préserver la biodiversité par des choix respectueux de l'environnement.</p>
+      <p class="text">{{ $t("badges.p7") }}</p>
+      <p class="text">{{ $t("badges.p7suite") }}</p>
     </div>
 
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge8.svg" alt="Image">
-      <p class="text">8. Nettoyeur Planétaire :<br>
-        Obtenu après la quête numéro 8, "Le défi de la gestion des déchets". Il met en lumière l'importance du tri et du recyclage pour réduire l'impact environnemental des déchets.</p>
+      <p class="text">{{ $t("badges.p8") }}</p>
+      <p class="text">{{ $t("badges.p8suite") }}</p>
     </div>
 
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge9.svg" alt="Image">
-      <p class="text">9. Allié des Océans :<br>
-        Ce badge est attribué à la fin de la quête numéro 9, "La surexploitation des ressources marines". Il honore ceux qui soutiennent la vie marine et la pêche durable pour protéger les océans.</p>
+      <p class="text">{{ $t("badges.p9") }}</p>
+      <p class="text">{{ $t("badges.p9suite") }}</p>
     </div>
 
     <div class="rounded-box">
       <img class="rounded-image" src="@/assets/badge/Badge10.svg" alt="Image">
-      <p class="text">10. Cultivateur des Terres Fertiles :<br>
-        Décerné après la quête numéro 10, "La dégradation et l'épuisement des sols". Ce badge valorise le soutien à l'agriculture durable et les efforts pour préserver la fertilité des sols.</p>
+      <p class="text">{{ $t("badges.p10") }}</p>
+      <p class="text">{{ $t("badges.p10suite") }}</p>
     </div>
   </main>
 </template>
