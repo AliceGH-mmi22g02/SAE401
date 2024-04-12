d2993ccde68abfa8c52d62041e87f0d4c9529457
SomebirdFaust <faust.gaulard@gmail.com>
2024-04-12 19:06:06 +0200

    diverses corrections

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <style lang="scss">
-@import "/public/css/scss_page/accueil";
+@import "src/assets/css/scss_page/accueil";
 /* Autres styles */
 </style>
 
@@ -11,18 +11,18 @@
   <main>
     <div class="intro">
       <div class="wellcome">
-      <h1>{{ $t("main.welcome") }} <strong>{{ $t("special.gp")}}</strong> {{ $t("special.exclam")}}</h1>
-      <p>{{ $t("main.p1") }}</p>
+        <h1>{{ $t("main.welcome") }} <strong>{{ $t("special.gp")}}</strong> {{ $t("special.exclam")}}</h1>
+        <p>{{ $t("main.p1") }}</p>
       </div>
 
       <div class="why">
         <div class="why-sign">
-          <img src="@/assets/icon.png" alt="pictogramme s'inscrire"/>
+          <img src="../../public/icon.png" alt="pictogramme s'inscrire"/>
           <h2>{{ $t("main.why") }}</h2>
           <p>{{ $t("main.p2") }}</p>
         </div>
         <div class="why-play">
-          <img src="@/assets/icon.png" alt="pictogramme jouer"/>
+          <img src="../../public/icon.png" alt="pictogramme jouer"/>
           <h2>{{ $t("main.whyplay") }}</h2>
           <p>{{ $t("main.p3") }}</p>
         </div>
@@ -33,27 +33,27 @@
       <h2>{{ $t("main.how") }}</h2>
       <div class="how-container">
         <div class="how">
-          <img src="@/assets/screenshot.png" alt="tuto selectionner une quête">
+          <img src="../../public/screenshot.png" alt="tuto selectionner une quête">
           <p>{{ $t("main.p4-2") }}</p>
         </div>
         <div class="how">
-          <img src="@/assets/screenshot.png" alt="tuto objectifs de la quête">
+          <img src="../../public/screenshot.png" alt="tuto objectifs de la quête">
           <p>{{ $t("main.p4-3") }}</p>
         </div>
       </div>
-      
+
       <h2>{{ $t("main.what") }}</h2>
       <div class="how-container">
         <div class="how">
-          <img src="@/assets/screenshot.png" alt="capture pop-up quête">
+          <img src="../../public/screenshot.png" alt="capture pop-up quête">
           <p>{{ $t("main.p5") }}</p>
         </div>
         <div class="how">
-          <img src="@/assets/screenshot.png" alt="capture quête">
+          <img src="../../public/screenshot.png" alt="capture quête">
           <p>{{ $t("main.p6") }}</p>
         </div>
         <div class="how">
-          <img src="@/assets/screenshot.png" alt="capture valider quête">
+          <img src="../../public/screenshot.png" alt="capture valider quête">
           <p>{{ $t("main.p7") }}</p>
         </div>
       </div>
@@ -62,26 +62,26 @@
     <div class="questsubject">
       <h2>{{ $t("main.subject") }}</h2>
       <div class="questsubject-container">
-      <div class="questsubject-column">
-        <img src="@/assets/picto/risques_climatique.svg" alt="picto risques climatiques"/>
-        <img src="@/assets/picto/consommation_des_ressources.svg" alt="picto consommation des ressources"/>
-      </div>  
-      <div class="questsubject-column">
-        <img src="@/assets/picto/deforestation.svg" alt="picto déforestation"/>
-        <img src="@/assets/picto/especes_en_voie_de_disparition.svg" alt="picto espèce en voie de disparition"/>
-        <img src="@/assets/picto/atteinte_a_la_biodiversite.svg" alt="picto atteinte à la biodiversité"/>
-      </div>
-      <div class="questsubject-column">
-        <img src="@/assets/picto/rechauffement_climatique.svg" alt="picto réchauffement climatique"/>
-        <img src="@/assets/picto/pollution.svg" alt="picto pollution"/>
-      </div>
+        <div class="questsubject-column">
+          <img src="../../public/picto/risques_climatique.svg" alt="picto risques climatiques"/>
+          <img src="../../public/picto/consommation_des_ressources.svg" alt="picto consommation des ressources"/>
+        </div>
+        <div class="questsubject-column">
+          <img src="../../public/picto/deforestation.svg" alt="picto déforestation"/>
+          <img src="../../public/picto/especes_en_voie_de_disparition.svg" alt="picto espèce en voie de disparition"/>
+          <img src="../../public/picto/atteinte_a_la_biodiversite.svg" alt="picto atteinte à la biodiversité"/>
+        </div>
+        <div class="questsubject-column">
+          <img src="../../public/picto/rechauffement_climatique.svg" alt="picto réchauffement climatique"/>
+          <img src="../../public/picto/pollution.svg" alt="picto pollution"/>
+        </div>
       </div>
     </div>
 
     <div class="recompense">
       <h2>{{ $t("main.p8") }}</h2>
       <h3>{{ $t("main.p9") }}</h3>
-      <img src="@/assets/screenshot.png">
+      <img src="../../public/screenshot.png">
       <div>
         <p v-html="$t('main.p10')"></p>
         <p v-html="$t('main.p11')"></p>
--- src/views/TableauQueteView.vue
+++ src/views/TableauQueteView.vue
@@ -1,16 +1,16 @@
 <template>
   <main @click="handleClick">
-    <img src="/src/assets/tableau_quete2.png" class="tableau" alt="Logo">
-    <a href="Information#info1" v-html="$t('images.quetes.1')"></a>
-    <a href="Information#info2" v-html="$t('images.quetes.2')"></a>
-    <a href="Information#info3" v-html="$t('images.quetes.3')"></a>
-    <a href="Information#info4" v-html="$t('images.quetes.4')"></a>
-    <a href="Information#info5" v-html="$t('images.quetes.5')"></a>
-    <a href="Information#info6" v-html="$t('images.quetes.6')"></a>
-    <a href="Information#info7" v-html="$t('images.quetes.7')"></a>
-    <a href="Information#info8" v-html="$t('images.quetes.8')"></a>
-    <a href="Information#info9" v-html="$t('images.quetes.9')"></a>
-    <a href="Information#info10" v-html="$t('images.quetes.10')"></a>
+    <img src="/public/tableau_quete2.png" class="tableau" alt="Logo">
+    <a href="Information#info1"><img id="quete1" class="quete1" src="/public/quete1.png" alt="Quête 1" data-quete="0" @click="handleClick($event.target)"></a>
+    <a href="Information#info2"><img id="quete2" class="quete2" src="/public/quete2.png" alt="Quête 2" data-quete="1" @click="handleClick($event.target)"></a>
+    <a href="Information#info3"><img id="quete3" class="quete3" src="/public/quete3.png" alt="Quête 3" data-quete="2" @click="handleClick($event.target)"></a>
+    <a href="Information#info4"><img id="quete4" class="quete4" src="/public/quete4.png" alt="Quête 4" data-quete="3" @click="handleClick($event.target)"></a>
+    <a href="Information#info5"><img id="quete5" class="quete5" src="/public/quete5.png" alt="Quête 5" data-quete="4" @click="handleClick($event.target)"></a>
+    <a href="Information#info6"><img id="quete6" class="quete6" src="/public/quete6.png" alt="Quête 6" data-quete="5" @click="handleClick($event.target)"></a>
+    <a href="Information#info7"><img id="quete7" class="quete7" src="/public/quete7.png" alt="Quête 7" data-quete="6" @click="handleClick($event.target)"></a>
+    <a href="Information#info8"><img id="quete8" class="quete8" src="/public/quete8.png" alt="Quête 8" data-quete="7" @click="handleClick($event.target)"></a>
+    <a href="Information#info9"><img id="quete9" class="quete9" src="/public/quete9.png" alt="Quête 9" data-quete="8" @click="handleClick($event.target)"></a>
+    <a href="Information#info10"><img id="quete10" class="quete10" src="/public/quete10.png" alt="Quête 10" data-quete="9" @click="handleClick($event.target)"></a>
     <PopUpQuete v-if="isPopUpVisible" :selectedQuete="selectedQuete" @closePopup="isPopUpVisible = false"></PopUpQuete>
   </main>
 </template>
@@ -29,7 +29,7 @@ const handleClick = (event) => {
 </script>
 
 <style scoped lang="scss">
-@import "/public/css/scss_page/tableauquete";
+@import "../assets/css/scss_page/tableauquete";
 </style>
 
 
